ef20bc345b2aa2186d04b750f638058c02b67e8b
ututehutuhetuh <86868292+ututehutuhetuh@users.noreply.github.com>
2022-06-17 10:17:10 +0800

    Update freshinstall.sh

--- archinstall/freshinstall.sh
+++ archinstall/freshinstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Syyu
 ln -sf /usr/share/zoneinfo/Asia/Kuching /etc/localtime
 hwclock --systohc
-sed -i '178s/.//' /etc/locale.gen
+sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=dvorak" >> /etc/vconsole.conf
@@ -15,7 +17,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 passwd
 
-pacman -S base-devel linux-headers networkmanager neovim git wget curl xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xf86-video-amdgpu amd-ucode grub efibootmgr htop wine-staging winetricks picom nitrogen ttf-font-awesome ttf-dejavu
+pacman -S base-devel linux-headers networkmanager neovim git wget curl xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon amd-ucode grub efibootmgr htop wine-staging winetricks picom nitrogen ttf-font-awesome ttf-dejavu pipewire pipewire-pulse pipewire-media-session firefox powerline-fonts
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 
@@ -26,6 +28,6 @@ systemctl enable NetworkManager
 useradd -mG wheel adji
 passwd adji
 
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/adji
+EDITOR=nvim visudo
 
 echo "if you use btrfs add "btrfs" to /etc/mkinitcpio.conf and don't forget to run mkinitcpio -p linux"
